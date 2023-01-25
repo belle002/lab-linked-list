82f5c9e5391476f894989422aa58390c2c3f3fed
Your Namebelle002 <you@example.com>
2023-01-24 19:48:39 -0500

    "TEST 3"

--- linkedlist.c
+++ linkedlist.c
@@ -7,13 +7,13 @@
 typedef struct  node_t 
 {
     int num_wins;
-    int ywear;
+    int year;
     struct node_t* next;
 } node_t;
    
 node_t* new_node(int wins, int year, node_t* next) {
 
-   node_t* newnode = (node_t*) malloc(sizeof(node_t));
+   node_t* newNode = (node_t*) malloc(sizeof(node_t));
 
    newNode->num_wins = wins;
    newNode->year = year;
@@ -26,7 +26,11 @@ int main()
 {
     node_t* head;
     head = new_node(10, 5, NULL);
-    printf("%d %d\n", head->year, head->num-wins);
-
+    node_t* new = new_node(1,2,NULL);
+    printf("%d %d\n", head->year, head->num_wins);
+    printf("%d %d\n", new->year, new->num_wins);
+    
+    head->next = new;
+    printf("%d %d\n", head->next->year, head->next->num_wins);
     return 0;
 }
